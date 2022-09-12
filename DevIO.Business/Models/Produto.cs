999558919eae70eda0cd5b25aa22710051b47a40
Alex Moura <alekissi@gmail.com>
2022-09-12 13:51:25 -0300

    primeiro commit

---
+++ DevIO.Business/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorID { get; set; }
+
+        public string Nome { get; set; }
+        
+        public string Descricao { get; set; }
+        
+        public string Imagem { get; set; }
+        
+        public decimal Valor { get; set; }
+        
+        public DateTime DataCadastro { get; set; }
+        
+        public bool Ativo { get; set; }
+
+        /* EF Ralation */
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
---
+++ src/DevIO.Data/Mappins/FornecedorMapping.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Data.Mappins
+{
+    class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f => f.Id);
+            
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+            
+            builder.Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            // 1 : 1 => Fornecedor : Endereco
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            // 1 : N => Fornecedor : Produto
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorID);
+
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
